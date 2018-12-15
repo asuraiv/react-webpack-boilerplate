616efff8c821c195129ee00fa1db2e8a71333028
asuraiv <asuraiv7@gmail.com>
2018-12-15 17:32:47 +0900

    reducer 기본 코드 추가

---
+++ src/react-redux-example/action/boardActionType.js
@@ -0,0 +1,4 @@
+export const CREATE = "CREATE";
+export const DELETE = "DELETE";
+export const READ = "READ";
+export const UPDATE = "UPDATE";
--- src/react-redux-example/components/layout/listItems.js
+++ src/react-redux-example/components/layout/listItems.js
@@ -11,29 +11,5 @@ export const mainListItems = (
 			</ListItemIcon>
 			<ListItemText primary="게시판" />
 		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<i className="material-icons">
-					location_city
-				</i>
-			</ListItemIcon>
-			<ListItemText primary="지자체 데이터 현황" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<i className="material-icons">
-					share
-				</i>
-			</ListItemIcon>
-			<ListItemText primary="버스노선" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<i className="material-icons">
-					directions_bus
-				</i>
-			</ListItemIcon>
-			<ListItemText primary="노선별 버스목록" />
-		</ListItem>
 	</div>
 );
